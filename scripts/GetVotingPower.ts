bc4e1891d0435110caf551a3318a9b089d046b07
hueldera <huelderantuness@gmail.com>
2024-06-16 19:08:30 -0300

    feat: Adapt scripts to TokenizedBallot

--- scripts/GetVotingPower.ts
+++ scripts/GetVotingPower.ts
@@ -1,6 +1,6 @@
-import { createPublicClient, http, hexToString } from "viem";
+import { createPublicClient, http } from "viem";
 import { sepolia } from "viem/chains";
-import { abi } from "../artifacts/contracts/TokenizedBallot.sol/Ballot.json";
+import { abi } from "../artifacts/contracts/MyToken.sol/MyToken.json";
 import * as dotenv from "dotenv";
 
 
@@ -9,46 +9,32 @@ const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 
 async function main() {
     const parameters = process.argv.slice(2);
-    if (!parameters || parameters.length < 1)
+    if (!parameters || parameters.length < 2)
         throw new Error("Parameters not provided");
+
     const contractAddress = parameters[0] as `0x${string}`;
     if (!contractAddress) throw new Error("Contract address not provided");
     if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
         throw new Error("Invalid contract address");
+
+    const voterAddress = parameters[1];
+    if (!voterAddress) throw new Error("Voter address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(voterAddress))
+        throw new Error("Invalid voter address");
 
     const publicClient = createPublicClient({
         chain: sepolia,
         transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
         });    
     
-    const winnerIndex = (await publicClient.readContract({
-      address: contractAddress,
-      abi,
-      functionName: "winningProposal",
-      args: [],
-    })) as number;
-
-    console.log("Winner proposal index: ", winnerIndex);
-
-    const winnerName = (await publicClient.readContract({
+    const voteCount = (await publicClient.readContract({
         address: contractAddress,
         abi,
-        functionName: "winnerName",
-        args: [],
+        functionName: "getVotes",
+        args: [voterAddress],
       })) as `0x${string}`;    
 
-    const name = hexToString(winnerName, { size: 32 });
-    console.log("Winner Proposal Name: ", name);
-
-    const proposal = (await publicClient.readContract({
-        address: contractAddress,
-        abi,
-        functionName: "proposals",
-        args: [BigInt(winnerIndex)],
-      })) as any[];
-  
-
-    console.log("Winner Proposal Vote Count: ", proposal[1]);
+    console.log("Voter Voting Power: ", voteCount);
 
     process.exit();
 }
