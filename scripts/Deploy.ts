ae8ca4ad622f606756492c2a8fea918b1442594a
Gustavo Dutra <gobannondutra@gmail.com>
2024-06-16 17:49:39 -0300

    Implemeting mint and deploy to the deployer

--- scripts/Deploy.ts
+++ scripts/Deploy.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, http, createWalletClient, formatEther, toHex, hexToString } from "viem";
+import { createPublicClient, http, createWalletClient, formatEther, parseEther, toHex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 import { abi as abiBallot, bytecode as bytecodeBallot } from "../artifacts/contracts/TokenizedBallot.sol/Ballot.json";
@@ -9,6 +9,8 @@ dotenv.config();
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 const deployerPrivateKey = process.env.PRIVATE_KEY || "";
 
+const MINT_VALUE = 1000n
+
 async function main() {
     const proposals = process.argv.slice(2);
     if (!proposals || proposals.length < 1)
@@ -17,60 +19,81 @@ async function main() {
     const publicClient = createPublicClient({
         chain: sepolia,
         transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
-      });
-      const blockNumber = await publicClient.getBlockNumber();
-      console.log("Last block number:", blockNumber);
+    });
+    const blockNumber = await publicClient.getBlockNumber();
+    console.log("Last block number:", blockNumber);
 
-      const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
-      const deployer = createWalletClient({
+    const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+    const deployer = createWalletClient({
         account,
         chain: sepolia,
         transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
-      });
-      console.log("Deployer address:", deployer.account.address);
-      const balance = await publicClient.getBalance({
+    });
+    console.log("Deployer address:", deployer.account.address);
+    const balance = await publicClient.getBalance({
         address: deployer.account.address,
-      });
-      console.log(
+    });
+    console.log(
         "Deployer balance:",
         formatEther(balance),
         deployer.chain.nativeCurrency.symbol
-      );
+    );
 
-      console.log("\nDeploying Token contract");
-      let hash = await deployer.deployContract({
+    console.log("\nDeploying Token contract");
+    let hash = await deployer.deployContract({
         abi: abiToken,
         bytecode: bytecodeToken as `0x${string}`,
         args: [],
-      });
-      console.log("Transaction hash:", hash);
-      console.log("Waiting for confirmations...");
-      let receipt = await publicClient.waitForTransactionReceipt({ hash });
-      const tokenAddress = receipt.contractAddress;
-      console.log("Token contract deployed to:", tokenAddress);     
-      
-      if(!tokenAddress) throw new Error("Contract address not found");
+    });
 
-      // TODO: mint tokens before deploying the ballot
+    console.log("Transaction hash:", hash);
+    console.log("Waiting for confirmations...");
+    let receipt = await publicClient.waitForTransactionReceipt({ hash });
+    const tokenAddress = receipt.contractAddress;
+    console.log("Token contract deployed to:", tokenAddress);
 
-      const lastBlockNumber = await publicClient.getBlockNumber();
-      const targetBlockNumber = lastBlockNumber - 100n;
-      console.log("\nDeploying Ballot contract");
-      hash = await deployer.deployContract({
+    if (!tokenAddress) throw new Error("Contract address not found");
+
+    console.log("Minting tokens for the deployer")
+    hash = await deployer.writeContract({
+        address: tokenAddress,
+        abi: abiToken,
+        functionName: "mint",
+        args: [deployer.account.address, MINT_VALUE],
+    });
+    console.log("Transaction hash:", hash);
+    console.log("Waiting for confirmations...");
+    await publicClient.waitForTransactionReceipt({ hash });
+
+    console.log("Delegating the voting power to the deployer")
+    hash = await deployer.writeContract({
+        address: tokenAddress,
+        abi: abiToken,
+        functionName: "delegate",
+        args: [deployer.account.address],
+    });
+    console.log("Transaction hash:", hash);
+    console.log("Waiting for confirmations...");
+    await publicClient.waitForTransactionReceipt({ hash });
+
+    const lastBlockNumber = await publicClient.getBlockNumber();
+    const targetBlockNumber = lastBlockNumber - 100n;
+    console.log("\nDeploying Ballot contract");
+    hash = await deployer.deployContract({
         abi: abiBallot,
         bytecode: bytecodeBallot as `0x${string}`,
         args: [
-          tokenAddress,
-          proposals.map((prop) => toHex(prop, { size: 32 })),
-          targetBlockNumber
+            proposals.map((prop) => toHex(prop, { size: 32 })),
+            tokenAddress,
+            targetBlockNumber
         ],
-      });
-      console.log("Transaction hash:", hash);
-      console.log("Waiting for confirmations...");
-      receipt = await publicClient.waitForTransactionReceipt({ hash });
-      console.log("Ballot contract deployed to:", receipt.contractAddress);     
-      
-      if(!receipt.contractAddress) throw new Error("Contract address not found");
+    });
+    console.log("Transaction hash:", hash);
+    console.log("Waiting for confirmations...");
+    receipt = await publicClient.waitForTransactionReceipt({ hash });
+    console.log("Ballot contract deployed to:", receipt.contractAddress);
+
+    if (!receipt.contractAddress) throw new Error("Contract address not found");
 }
 
 main().catch((error) => {
